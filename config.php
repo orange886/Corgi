8a460fea041b880f8a453a4f46ee2c2827a5a310
fangcaicheng04 <fangcaicheng04@gmail.com>
2024-05-27 12:43:57 +0000

    修改

--- config.php
+++ config.php
@@ -2,12 +2,12 @@
 if (!defined('CSCMSPATH')) exit('No permission resources');
 
 return array(
-	'mid' => '154',                                  //云平台ID（开发者可以留空）
-	'dir' => 'vvvdj',                            //模板唯一标示（模板目录名）
-	'path' => 'vvvdj/vdjcsfjdz1547/',                     //模板安装目录
-	'name' => 'vvvdj清风dj模板',                      //模板名称
-	'author' => '小橙子',                           //模板作者
-	'version' => 'v4.1',                           //模板版本
-	'description' => "小橙子" //模板版权/介绍
+	'mid' => '',                                  //云平台ID（开发者可以留空）
+	'dir' => 'Corgi',                             //模板唯一标示（模板目录名）
+	'path' => 'Corgi/Corgi0401/',             //模板安装目录
+	'name' => 'Corgi模板',                        //模板名称
+	'author' => 'orange',                         //模板作者
+	'version' => 'v1.0',                         //模板版本
+	'description' => "Corgi模板"                 //模板版权/介绍
 );
 
